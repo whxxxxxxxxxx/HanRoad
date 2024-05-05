c1fc1474b65a3975201caf1fbfb9d0f3e70be73c
Eikoo <114289969+whxxxxxxxxxx@users.noreply.github.com>
2024-05-05 18:20:37 +0800

    'init'

---
+++ 丝路汉韵/src/scripts/index.ts
@@ -0,0 +1,7 @@
+import game from "./game.narrat";
+import game1 from "./game1.narrat";
+import game2 from "./game2.narrat";
+import game3 from "./game3.narrat";
+import game4 from "./game4.narrat";
+import roadFinding from "./roadFinding.narrat";
+export default [game, game1, game4, game2, game3, roadFinding];
